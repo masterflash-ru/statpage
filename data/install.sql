c19a9a3500f0d693ec3ba0456a0806f678675be4
Masterflash <sxq@yandex.ru>
2018-04-24 09:38:29 +0300

    f

--- data/install.sql
+++ data/install.sql
@@ -15,8 +15,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-delete from design_tables where table_name='statpage';
-delete from design_tables where table_name='statpage_text';
 INSERT INTO `design_tables` (`interface_name`, `table_name`, `table_type`, `col_name`, `caption_style`, `row_type`, `col_por`, `pole_spisok_sql`, `pole_global_const`, `pole_prop`, `pole_type`, `pole_style`, `pole_name`, `default_sql`, `functions_befo`, `functions_after`, `functions_befo_out`, `functions_befo_del`, `properties`, `value`, `validator`, `sort_item_flag`, `col_function_array`) VALUES 
   ('statpage', 'statpage', 0, 'sysname', '', 3, 0, '', '', 'size=80', '2', '', 'sysname', '', '', '', '', '', 'N;', '', 'N;', 0, 'N;'),
   ('statpage', 'statpage', 0, 'tpl', '', 2, 5, '', '', '', '4', '', 'tpl', '', '', '', '\\Mf\\Statpage\\Lib\\Func\\GetTplList', '', 'a:2:{i:0;s:1:\"0\";i:1;s:1:\"1\";}', '', 'N;', 0, 'N;'),
@@ -26,12 +24,12 @@ INSERT INTO `design_tables` (`interface_name`, `table_name`, `table_type`, `col_
   ('statpage', 'statpage', 0, 'keywords', '', 2, 11, '', '', 'cols=130 rows=3', '3', '', 'keywords', '', '', '', '', '', 'N;', '', 'N;', 0, 'N;'),
   ('statpage', 'statpage', 0, 'keywords', '', 3, 0, '', '', 'cols=130 rows=3', '3', '2', 'keywords', '', '', '', '', '', 'N;', '', 'N;', 0, 'N;'),
   ('statpage', 'statpage', 0, 'description', '', 2, 12, '', '', 'cols=130 rows=3', '3', '', 'description', '', '', '', '', '', 'N;', '', 'N;', 0, 'N;'),
-  ('statpage', 'statpage', 0, 'name,content,keywords,description,title,tpl,page_type', '', 0, 1, 'select statpage.*,\r\n(select title from statpage_text where locale=''$pole_dop0'' and statpage_text.statpage=statpage.id) as title ,\r\n(select keywords from statpage_text where locale=''$pole_dop0'' and statpage_text.statpage=statpage.id) as keywords,\r\n(select description from statpage_text where locale=''$pole_dop0'' and statpage_text.statpage=statpage.id) as description,\r\n(select content from statpage_text where locale=''$pole_dop0'' and statpage_text.statpage=statpage.id) as content,\r\n(select tpl from statpage_text where locale=''$pole_dop0'' and statpage_text.statpage=statpage.id) as tpl,\r\n(select page_type from statpage_text where locale=''$pole_dop0'' and statpage_text.statpage=statpage.id) as page_type\r\n\r\nfrom statpage order by name', '', '0,0,0,0', 'name', '0', 'id', 'delete from statpage  where id=$id;\r\ndelete from statpage_text where statpage=$id', '', '', '', '', 'Mf\Statpage\\Lib\\Func\\Save', 0x613A323A7B733A32343A22666F726D5F656C656D656E74735F6E65775F7265636F7264223B733A313A2231223B733A32343A22666F726D5F656C656D656E74735F6A6D705F7265636F7264223B733A313A2231223B7D, 'statpage', 1, ''),
+  ('statpage', 'statpage', 0, 'name,content,keywords,description,title,tpl,page_type', '', 0, 1, 'select statpage.*,\r\n(select title from statpage_text where locale=''$pole_dop0'' and statpage_text.statpage=statpage.id) as title ,\r\n(select keywords from statpage_text where locale=''$pole_dop0'' and statpage_text.statpage=statpage.id) as keywords,\r\n(select description from statpage_text where locale=''$pole_dop0'' and statpage_text.statpage=statpage.id) as description,\r\n(select content from statpage_text where locale=''$pole_dop0'' and statpage_text.statpage=statpage.id) as content,\r\n(select tpl from statpage_text where locale=''$pole_dop0'' and statpage_text.statpage=statpage.id) as tpl,\r\n(select page_type from statpage_text where locale=''$pole_dop0'' and statpage_text.statpage=statpage.id) as page_type\r\n\r\nfrom statpage order by name', '', '0,0,0,0', 'name', '0', 'id', 'delete from statpage  where id=$id;\r\ndelete from statpage_text where statpage=$id', '', '', '', '', 'Mf\\Statpage\\Lib\\Func\\Save', 0x613A323A7B733A32343A22666F726D5F656C656D656E74735F6E65775F7265636F7264223B733A313A2231223B733A32343A22666F726D5F656C656D656E74735F6A6D705F7265636F7264223B733A313A2231223B7D, 'statpage', 1, ''),
   ('statpage', 'statpage', 0, '', '', 1, 0, '', '', 'onChange=this.form.submit()', '4', '', '', '', '', '', '\\Mf\\Statpage\\Lib\\Func\\GetLocales', '', 'a:2:{i:0;s:1:\"0\";i:1;s:1:\"0\";}', '', '', 0, ''),
   ('statpage', 'statpage', 0, 'name', '', 2, 1, '', '', 'size=80', '2', '', 'name', '', '', '', '', '', 'N;', '', 'N;', 0, 'N;'),
   ('statpage', 'statpage', 0, 'name', '', 3, 0, '', '', 'size=80', '2', '2', 'name', '', '', '', '', '', 'N;', '', 'N;', 0, 'N;'),
-  ('statpage', 'statpage', 0, 'content', '', 2, 6, '', '[''public_media_folder'']', ',', '39', '', 'content', '', '', '\\Mf\\Statpage\\Lib\\Func\\ClearContent', '', '', 'a:4:{i:0;s:3:\"600\";i:1;s:3:\"800\";i:2;s:7:\"default\";i:3;s:7:\"default\";}', '', 'N;', 0, 'N;'),
-  ('statpage', 'statpage', 0, 'content', '', 3, 0, '', '[''public_media_folder'']', ',', '39', '', 'content', '', '', '\\Mf\\Statpage\\Lib\\Func\\ClearContent', '', '', 'a:4:{i:0;s:3:\"600\";i:1;s:3:\"800\";i:2;s:7:\"default\";i:3;s:7:\"default\";}', '', 'N;', 0, 'N;'),
+  ('statpage', 'statpage', 0, 'content', '', 2, 6, '', '[\"statpage\"][''media_folder'']', ',', '39', '', 'content', '', '', '\\Mf\\Statpage\\Lib\\Func\\ClearContent', '', '', 'a:4:{i:0;s:3:\"600\";i:1;s:3:\"800\";i:2;s:7:\"default\";i:3;s:7:\"default\";}', '', 'N;', 0, 'N;'),
+  ('statpage', 'statpage', 0, 'content', '', 3, 0, '', '[\"statpage\"][''media_folder'']', ',', '39', '', 'content', '', '', '\\Mf\\Statpage\\Lib\\Func\\ClearContent', '', '', 'a:4:{i:0;s:3:\"600\";i:1;s:3:\"800\";i:2;s:7:\"default\";i:3;s:7:\"default\";}', '', 'N;', 0, 'N;'),
   ('statpage', 'statpage', 0, 'description', '', 3, 0, '', '', 'cols=130 rows=3', '3', '2', 'description', '', '', '', '', '', 'N;', '', 'N;', 0, 'N;'),
   ('statpage', 'statpage', 0, 'url', '', 2, 2, '', '', 'size=120', '2', '', 'url', '', '\\Mf\\Statpage\\Lib\\Func\\CreateUrl', '', '', '', 'N;', '', 'N;', 0, 'N;'),
   ('statpage', 'statpage', 0, 'url', '', 3, 0, '', '', 'size=120', '2', '', 'url', '', '\\Mf\\Statpage\\Lib\\Func\\CreateUrl', '', '', '', 'N;', '', 'N;', 0, 'N;'),
@@ -75,7 +73,7 @@ CREATE TABLE `statpage` (
   PRIMARY KEY (`id`),
   KEY `sysname` (`sysname`),
   KEY `url` (`url`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,9 +92,10 @@ CREATE TABLE `statpage_text` (
   `tpl` char(127) DEFAULT NULL COMMENT 'имя шаблона-контейнера',
   `page_type` int(11) DEFAULT NULL COMMENT 'типа страницы: 1-публиковать на сайте, 0-нет, 2-внутренняя',
   `content` text COMMENT 'контент страницы',
-  `lastmod` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`statpage`,`locale`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `lastmod` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`statpage`,`locale`),
+   CONSTRAINT `statpage_text_fk` FOREIGN KEY (`statpage`) REFERENCES `statpage` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
