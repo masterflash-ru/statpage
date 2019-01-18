01a5d8e30df22f25665a32cc16ea9044cc9e28c0
Masterflash <sxq@yandex.ru>
2019-01-18 17:37:10 +0300

    Update GetPageType.php
    добавлена сортировка для удобства

--- src/Lib/Func/GetPageType.php
+++ src/Lib/Func/GetPageType.php
@@ -9,12 +9,11 @@ public function __invoke($obj,$infa,$struct_arr,$col_number,$pole_dop,$tab_name,
 {
 
 	$s=$obj->config["statpage"]["status"];
-
-	foreach ($s as $status=>$name)
-		{
-			$obj->dop_sql['name'][]=$name;		
-			$obj->dop_sql['id'][]=$status;
-		}
+    ksort($s);
+	foreach ($s as $status=>$name){
+        $obj->dop_sql['name'][]=$name;
+        $obj->dop_sql['id'][]=$status;
+    }
 
 	return $infa;
 }
