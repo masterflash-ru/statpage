c3d78bf742b0599e03605f922a9d2995e1b3fa12
Masterflash <sxq@yandex.ru>
2019-11-04 16:16:16 +0300

    Update Version20191104155459.php

--- migrations/Version20191104155459.php
+++ migrations/Version20191104155459.php
@@ -11,31 +11,47 @@ class Version20191104155459 extends AbstractMigration implements MigrationInterf
 
     public function up($schema)
     {
-        $this->addSql("CREATE TABLE `statpage` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
-          `name` char(255) DEFAULT NULL,
-          `sysname` char(127) DEFAULT NULL,
-          `url` char(255) DEFAULT NULL,
-          `locale` char(5) DEFAULT NULL,
-          `tpl` char(50) DEFAULT NULL,
-          `page_type` int(11) DEFAULT NULL,
-          `lastmod` datetime NOT NULL,
-          `seo_options` varchar(2000) DEFAULT NULL,
-          `layout` char(127) DEFAULT NULL,
-          `content` text,
-          `title` char(255) DEFAULT NULL,
-          `keywords` char(255) DEFAULT NULL,
-          `description` text,
-          PRIMARY KEY (`id`),
-          UNIQUE KEY `url` (`url`),
-          KEY `sysname` (`sysname`),
-          KEY `locale` (`locale`),
-          KEY `page_type` (`page_type`)
-        ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='статичные страницы'");
+        switch ($this->db_type){
+            case "mysql":{
+                $this->addSql("CREATE TABLE `statpage` (
+                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                  `name` char(255) DEFAULT NULL,
+                  `sysname` char(127) DEFAULT NULL,
+                  `url` char(255) DEFAULT NULL,
+                  `locale` char(5) DEFAULT NULL,
+                  `tpl` char(50) DEFAULT NULL,
+                  `page_type` int(11) DEFAULT NULL,
+                  `lastmod` datetime NOT NULL,
+                  `seo_options` varchar(2000) DEFAULT NULL,
+                  `layout` char(127) DEFAULT NULL,
+                  `content` text,
+                  `title` char(255) DEFAULT NULL,
+                  `keywords` char(255) DEFAULT NULL,
+                  `description` text,
+                  PRIMARY KEY (`id`),
+                  UNIQUE KEY `url` (`url`),
+                  KEY `sysname` (`sysname`),
+                  KEY `locale` (`locale`),
+                  KEY `page_type` (`page_type`)
+                ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='статичные страницы'");
+                break;
+            }
+            default:{
+                throw new \Exception("the database {$this->db_type} is not supported !");
+            }
+        }
     }
 
     public function down($schema)
     {
-        $this->addSql("drop table `statpage`");
+        switch ($this->db_type){
+            case "mysql":{
+                $this->addSql("drop table `statpage`");
+                break;
+            }
+            default:{
+                throw new \Exception("the database {$this->db_type} is not supported !");
+            }
+        }
     }
 }
