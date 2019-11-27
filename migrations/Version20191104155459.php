606ac627156d160cf04a617c09b25ba24e17e17f
Masterflash <sxq@yandex.ru>
2019-11-27 11:40:30 +0300

    Update Version20191104155459.php

--- migrations/Version20191104155459.php
+++ migrations/Version20191104155459.php
@@ -12,35 +12,38 @@ class Version20191104155459 extends AbstractMigration implements MigrationInterf
 
     public function up($schema,$adapter)
     {
-        switch ($this->db_type){
-            case "mysql":{
-                $this->addSql("CREATE TABLE `statpage` (
-                  `id` int(11) NOT NULL AUTO_INCREMENT,
-                  `name` char(255) DEFAULT NULL,
-                  `sysname` char(127) DEFAULT NULL,
-                  `url` char(255) DEFAULT NULL,
-                  `locale` char(5) DEFAULT NULL,
-                  `tpl` char(50) DEFAULT NULL,
-                  `page_type` int(11) DEFAULT NULL,
-                  `lastmod` datetime NOT NULL,
-                  `seo_options` varchar(2000) DEFAULT NULL,
-                  `layout` char(127) DEFAULT NULL,
-                  `content` text,
-                  `title` char(255) DEFAULT NULL,
-                  `keywords` char(255) DEFAULT NULL,
-                  `description` text,
-                  PRIMARY KEY (`id`),
-                  UNIQUE KEY `url` (`url`),
-                  KEY `sysname` (`sysname`),
-                  KEY `locale` (`locale`),
-                  KEY `page_type` (`page_type`)
-                ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='статичные страницы'");
-                break;
-            }
-            default:{
-                throw new \Exception("the database {$this->db_type} is not supported !");
-            }
-        }
+        $this->mysql_add_create_table=" ENGINE=MyIsam DEFAULT CHARSET=utf8";
+        $table = new Ddl\CreateTable("statpage");
+        $table->addColumn(new Ddl\Column\Integer('id',false,null,["AUTO_INCREMENT"=>true]));
+        $table->addColumn(new Ddl\Column\Char('name', 255,true,null,["COMMENT"=>"Имя страницы"]));
+        $table->addColumn(new Ddl\Column\Char('sysname', 255,true,null,["COMMENT"=>"Системное имя страницы"]));
+        $table->addColumn(new Ddl\Column\Char('url', 255,true,null,["COMMENT"=>"URL"]));
+        $table->addColumn(new Ddl\Column\Char('locale', 5,true,null,["COMMENT"=>"Локаль формата ru_RU","KEY"=>"locale"]));
+        $table->addColumn(new Ddl\Column\Char('tpl', 50,true,null,["COMMENT"=>"Шаблон вывода"]));
+        $table->addColumn(new Ddl\Column\Integer('page_type',true,null,["COMMENT"=>"Код типа страницы"]));
+        $table->addColumn(new Ddl\Column\Datetime('lastmod', true,null,["COMMENT"=>"дата модификации"]));
+        $table->addColumn(new Ddl\Column\Varchar('seo_options', 2000,true,null,["COMMENT"=>"SEO опции"]));
+        $table->addColumn(new Ddl\Column\Varchar('layout', 127,true,null,["COMMENT"=>"Макет layout"]));
+        $table->addColumn(new Ddl\Column\Text('content', 0,true,null,["COMMENT"=>"контент страницы"]));
+        $table->addColumn(new Ddl\Column\Char('title', 255,true,null));
+        $table->addColumn(new Ddl\Column\Char('keywords', 255,true,null));
+        $table->addColumn(new Ddl\Column\Varchar('description', 3000,true,null));
+        $table->addConstraint(
+            new Ddl\Constraint\PrimaryKey(['id'])
+        );
+        $table->addConstraint(
+            new Ddl\Constraint\UniqueKey(['url'])
+        );
+        $table->addConstraint(
+            new Ddl\Index\Index(['sysname'],'sysname')
+        );
+        $table->addConstraint(
+            new Ddl\Index\Index(['locale'],'locale')
+        );
+        $table->addConstraint(
+            new Ddl\Index\Index(['page_type'],'page_type')
+        );
+        $this->addSql($table);
     }
 
     public function down($schema,$adapter)
